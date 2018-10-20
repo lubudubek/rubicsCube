48593f4c20799cff89ba290c5b73b30884c44e2c
Piotr Kleszcz <piokleszcz@gmail.com>
2018-10-20 23:30:11 +0200

    before test framework - multiple objects done

--- podejscieDrugie/main.cpp
+++ podejscieDrugie/main.cpp
@@ -12,6 +12,8 @@
 #include "Texture.hpp"
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
 
 int main() {
 	// start GL context and O/S window using the GLFW helper library
@@ -44,10 +46,10 @@ int main() {
 	//glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
 
 	float points[] = {
-	100.0f, 100.0f, 0.2f, 0.2f,
-	200.0f, 100.0f, 0.8f, 0.2f,
-	200.0f, 200.0f, 0.8f, 0.8f,
-	100.0f, 200.0f, 0.2f, 0.8f
+	-50.0f, -50.0f, 0.2f, 0.2f,
+	 50.0f, -50.0f, 0.8f, 0.2f,
+	 50.0f,  50.0f, 0.8f, 0.8f,
+	-50.0f,  50.0f, 0.2f, 0.8f
 	};
 
 	unsigned int indices[] = {
@@ -68,34 +70,57 @@ int main() {
 	IndexBuffer ib(indices, 6);
 
 	glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100.0f, 0.0f, 0.0f));
-	glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200.0f, 200.0f, 0.0f));
+	glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
+
 
 	Shader shader("res/shaders/Basic.shader");
 
-	glm::mat4 mvp = proj * view * model;
 
 	shader.Bind();
-	shader.SetUniform1i("u_Texture", 2);
-	shader.SetUniformMat4f("u_MVP", mvp);
+	shader.SetUniform1i("u_Texture", 0);
 	
 	Texture texture("logo1.png");
-	texture.Bind(2);
+	texture.Bind();
 	//shader.SetUniform1i("u_Texture", 2);
+
 
 	float r = 0.0f;
 	Renderer render;
+
+	ImGui::CreateContext();
+	ImGui_ImplGlfwGL3_Init(window, true);
+	ImGui::StyleColorsDark();
+	glm::vec3 translationA(200.0f, 200.0f, 0.0f);
+	glm::vec3 translationB(400.0f, 200.0f, 0.0f);
+
+	//glm::mat4 model;
+	//glm::mat4 mvp;
+
 	float increment = 0.05f;
 	while (!glfwWindowShouldClose(window)) {
 		render.Clear();
+
+		ImGui_ImplGlfwGL3_NewFrame();
 		// wipe the drawing surface clear
-		shader.Bind();
-		
+
 		//shader.SetUniform4f("p_color", r, 0.4f, 0.2f, 1.0f);
-		
-		render.Draw(va, ib, shader);
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
+			glm::mat4 mvp = proj * view * model;
+			shader.Bind();
+
+			shader.SetUniformMat4f("u_MVP", mvp);
+			render.Draw(va, ib, shader);
 		// update other events like input handling 
-		glfwPollEvents();
+		}
+		{
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
+			glm::mat4 mvp = proj * view * model;
+			shader.Bind();
+
+			shader.SetUniformMat4f("u_MVP", mvp);
+			render.Draw(va, ib, shader);
+		}
 
 		if (r > 1.0f)
 			increment = -0.05f;
@@ -103,8 +128,22 @@ int main() {
 			increment = 0.05f;
 		r += increment;
 		// put the stuff we've been drawing onto the display
+		{
+			ImGui::SliderFloat3("float A", &translationA.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			ImGui::SliderFloat3("float B", &translationB.x, 0.0f, 960.0f);            // Edit 1 float using a slider from 0.0f to 1.0f    
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		}
+
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(window);
+
+		glfwPollEvents();
 	}
+
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
 
 	// close GL context and any other GLFW resources
 	glfwTerminate();
